4cd98b7b5e95f76e550152e30ada2d48b56470fc
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-11-10 19:22:49 +0500

    Task7

---
+++ Tyuiu.LyaginVA.Sprint4.Task7.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.LyaginVA.Sprint4.Task7.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.LyaginVA.Sprint4.Task7.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0746ba49-1a4a-431a-b7ab-439386a757bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.LyaginVA.Sprint4.Task7.V6/Program.cs
+++ Tyuiu.LyaginVA.Sprint4.Task7.V6/Program.cs
@@ -3,49 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.LyaginVA.Sprint4.Task6.V5.Lib;
+using Tyuiu.LyaginVA.Sprint4.Task7.V6.Lib;
 
-namespace Tyuiu.LyaginVA.Sprint4.Task6.V5
+namespace Tyuiu.LyaginVA.Sprint4.Task7.V6
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+
+            string str = "458963214789";
             DataService ds = new DataService();
             Console.Title = "Спринт #4 | Выполнил: Лягин В. Л. | АСОиУБ-23-2";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
-            Console.WriteLine("* Задание #6                                                              *");
-            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
             Console.WriteLine("* Выполнил: Лягин Владимир Александрович | АСОиУБ-23-2                    *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дан строковый массив данных [Меркурий, Венера, Земля, Марс, Юпитер,     *");
-            Console.WriteLine("* Сатурн, Уран, Нептун] используя класс Array подсчитайте количество      *");
-            Console.WriteLine("* элементов, длина которых равна 5.                                       *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр: 458963214789. Преобразуйте ее в      *");
+            Console.WriteLine("* матрицу 3 на 4 и подсчитайте сумму нечетных чисел.                      *");
             Console.WriteLine("*                                                                         *");
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
             Console.WriteLine("***************************************************************************");
+            int index = 0;
 
-            var planets = new string[] { "Меркурий", "Венера", "Земля", "Марс", "Юпитер", "Сатурн", "Уран", "Нептун" };
-
-            Console.WriteLine("Исходный массив:");
-            for (int i = 0; i <= planets.Length - 1; i++)
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
             {
-                Console.WriteLine(planets[i]);
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
             }
+            Console.WriteLine();
+
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("Количество элементов длинна которых = 5:");
+            int res = ds.Calculate(rows, columns, str);
 
-            int nums = ds.Calculate(planets);
-
-            Console.WriteLine(nums);
+            Console.WriteLine("Сумма нечетных элементов = " + res);
             Console.ReadKey();
+
+
 
         }
     }
